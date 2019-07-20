8bdf223901dabaffb584eaf6eec15e3b7ed95c18
surveer-mazumdar <45826935+surveer-mazumdar@users.noreply.github.com>
2019-07-20 20:19:22 +0530

    Data Structures And Algorithms

---
+++ Data-Structures/selection_sort.js
@@ -0,0 +1,20 @@
+function SelectionSort(arr){
+    for(var i  = 0; i< arr.length; i++){
+        var lowest = i;
+        for(var j = i+1; j < arr.length; j++){
+            if(arr[j] < arr[lowest]){
+                lowest = j;
+            }
+        }
+
+        if(i !== lowest){
+            var temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+}
+
+var arr3 =  [33, 12, 45, 23, 78, 56, 43, 32];
+SelectionSort(arr3);
+console.log(arr3);
